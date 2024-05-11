e0c9a30fe68a4e266439011757e0e57723b6d98e
Vishruth Pobbathi <115341957+vishruthp2003@users.noreply.github.com>
2024-05-11 15:05:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function fetchWeather() {
+    const city = document.getElementById("cityInput").value;
+    const apiKey = '132b831805516a46affa0ea3bab16fda';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            document.getElementById("weatherInfo").innerText = "Error fetching weather data. Please try again.";
+        });
+}
+
+function displayWeather(data) {
+    const weatherInfo = document.getElementById("weatherInfo");
+    weatherInfo.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Description: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+}
